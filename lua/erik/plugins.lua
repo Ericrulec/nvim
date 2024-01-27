f78211a9911ba38aa0c0bec25502f1bb360ce5d6
Erik Jensson <erik@jensson.no>
2024-01-27 02:09:53 +0100

    changes to pylsp

--- lua/erik/plugins.lua
+++ lua/erik/plugins.lua
@@ -11,8 +11,8 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+vim.g.mapleader = "U+005C"
+vim.g.maplocalleader = "U+005C"
 
 local status, lazy = pcall(require, "lazy")
 if (not status) then
