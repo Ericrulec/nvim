64641afbd6e9fbf404ef5cbf0f31f94118c9b254
Erik Jensson <erik@jensson.no>
2024-03-18 21:25:37 +0100

    disable pylint - annoying "hints" everywhere

--- plugin/lspconfig.lua
+++ plugin/lspconfig.lua
@@ -94,7 +94,7 @@ nvim_lsp.pylsp.setup {
                 autopep8 = { enabled = false },
                 yapf = { enabled = false },
                 -- linter options
-                pylint = { enabled = true, executable = "pylint" },
+                pylint = { enabled = false, executable = "pylint" },
                 pyflakes = { enabled = false },
                 pycodestyle = { enabled = false },
                 -- type checker
